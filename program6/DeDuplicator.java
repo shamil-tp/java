63e4ab40a8fc7e1cae1ad30bbbcc75ab3c48d441
shamil <for.photos.only001@gmail.com>
2026-02-08 23:52:53 +0530

    Array De Duplicator

---
+++ program6/DeDuplicator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class DeDuplicator {
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        for(int i = 0;i<arr.length;i++){
+            System.out.println("Enter the value of element "+i);
+            arr[i] = scn.nextInt();
+            for(int j = 0;j<i;j++)
+        }
+    }
+}

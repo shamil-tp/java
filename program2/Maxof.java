b693c0bfc47ddd0e4d41600972c5ab43eaa4d47c
shamil <for.photos.only001@gmail.com>
2026-02-08 13:50:59 +0530

    program1 & 2 completed

---
+++ program2/Maxof.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Maxof{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x ;
+        System.out.println("enter a number");
+        x = scn.nextInt();
+
+        System.out.println("the value for x:" + x);
+        scn.close();
+    }
+}

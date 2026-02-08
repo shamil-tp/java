6b02408806044740786e3c331f7260bb3601d77e
shamil <for.photos.only001@gmail.com>
2026-02-08 23:57:01 +0530

    Array De Duplicator

--- program6/DeDuplicator.java
+++ program6/DeDuplicator.java
@@ -9,7 +9,18 @@ class DeDuplicator {
         for(int i = 0;i<arr.length;i++){
             System.out.println("Enter the value of element "+i);
             arr[i] = scn.nextInt();
-            for(int j = 0;j<i;j++)
+            for(int j = 0;j<i;j++){
+                if(arr[j] == arr[i]){
+                    arr[j] = 0; 
+                }
+            }
         }
+
+            System.out.print("\nfinal array is: ");
+
+        for(int k = 0;k<arr.length;k++){
+            System.out.print(k);
+        }
+        scn.close();
     }
 }
